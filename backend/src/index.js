ee4dce79648ea4175f6a25ab90365fbd9240203c
Augusto Renan <augustorenanss@gmail.com>
2020-06-05 13:55:52 -0300

    Pequenos ajustes

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const routes = require("./routes");
 const cors = require("cors");
+const { errors } = require("celebrate");
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 const PORT = 3000;
 app.listen(PORT, () => {
